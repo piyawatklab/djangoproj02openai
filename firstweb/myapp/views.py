f5d9f29d4fb55643238a2c389282cace66bc5731
piyawatklab <bobbybansong@gmail.com>
2024-02-15 13:27:23 +0700

    add env

--- firstweb/myapp/views.py
+++ firstweb/myapp/views.py
@@ -9,13 +9,15 @@ api_key = os.getenv("OPENAI_KEY", None)
 openai.api_key = api_key
 
 def Home(request):
-    print(api_key)
-    print(request.method)
-    if api_key is not None and request.method == 'POST':
-        user_imput = request.POST.get('user_imput')
-        response = openai.Image.create(
-            prompt = user_imput,
-            size = '256x256'
-        )
-        print(response)
+    user_input = request.POST.get('user_input', None)  # แก้ชื่อของฟิลด์ให้ถูกต้อง
+    print(user_input)  # แก้ชื่อตัวแปรให้ถูกต้อง
+    if api_key is not None and request.method == 'POST' and user_input:  # ตรวจสอบว่ามีข้อมูลที่ผู้ใช้ป้อนหรือไม่
+        try:
+            response = openai.Image.create(
+                prompt = user_input,
+                size = '256x256'
+            )
+            print(response)
+        except Exception as e:
+            print(e)
     return render(request,"myapp/main.html", {})
